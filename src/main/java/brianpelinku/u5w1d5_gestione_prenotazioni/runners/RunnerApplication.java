a487724764600778a646ae94c9dcc4dbb1a0860a
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-06 12:57:04 +0200

    create entità, repositories, services e runner dell'applicazione

--- src/main/java/brianpelinku/u5w1d5_gestione_prenotazioni/runners/RunnerApplication.java
+++ src/main/java/brianpelinku/u5w1d5_gestione_prenotazioni/runners/RunnerApplication.java
@@ -1,4 +1,32 @@
 package brianpelinku.u5w1d5_gestione_prenotazioni.runners;
 
-public class RunnerApplication {
+import brianpelinku.u5w1d5_gestione_prenotazioni.services.EdificioService;
+import brianpelinku.u5w1d5_gestione_prenotazioni.services.PostazioneAziendaleService;
+import brianpelinku.u5w1d5_gestione_prenotazioni.services.PrenotazioneService;
+import brianpelinku.u5w1d5_gestione_prenotazioni.services.UtenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RunnerApplication implements CommandLineRunner {
+
+    @Autowired
+    private EdificioService edificioService;
+
+    @Autowired
+    private PostazioneAziendaleService postazioneAziendaleService;
+
+    @Autowired
+    private PrenotazioneService prenotazioneService;
+
+    @Autowired
+    private UtenteService utenteService;
+
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
