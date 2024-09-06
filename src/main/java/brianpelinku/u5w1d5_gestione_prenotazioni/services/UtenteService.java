03bd94a7786c9ae5523d820f3e27c7f28e639247
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-06 14:03:26 +0200

    Update UtenteService.java

--- src/main/java/brianpelinku/u5w1d5_gestione_prenotazioni/services/UtenteService.java
+++ src/main/java/brianpelinku/u5w1d5_gestione_prenotazioni/services/UtenteService.java
@@ -1,5 +1,8 @@
 package brianpelinku.u5w1d5_gestione_prenotazioni.services;
 
+import brianpelinku.u5w1d5_gestione_prenotazioni.entities.Utente;
+import brianpelinku.u5w1d5_gestione_prenotazioni.exceptions.NotFoundException;
+import brianpelinku.u5w1d5_gestione_prenotazioni.exceptions.ValidationException;
 import brianpelinku.u5w1d5_gestione_prenotazioni.repositories.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,20 @@ import org.springframework.stereotype.Service;
 public class UtenteService {
     @Autowired
     private UtenteRepository utenteRepository;
+
+    public void saveUtente(Utente utente) {
+        if (utenteRepository.existsByUsername(utente.getUsername()))
+            throw new ValidationException("L'utente " + utente.getUsername() + " già esistente nel DB");
+        utenteRepository.save(utente);
+    }
+
+    public Utente findById(long utenteId) {
+        return utenteRepository.findById(utenteId).orElseThrow(() -> new NotFoundException(utenteId));
+    }
+
+    public void findByIdAndDelete(long utenteId) {
+        Utente found = this.findById(utenteId);
+        utenteRepository.delete(found);
+        System.out.println("L'utente con id " + utenteId + " è stato eliminato correttamente dal DB.");
+    }
 }
